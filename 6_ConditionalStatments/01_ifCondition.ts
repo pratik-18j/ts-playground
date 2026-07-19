f72329cc5711b7505b86dae467033ded7f5abd6d
pratik-18j <jpartik1891@gmail.com>
2026-07-19 17:04:40 +0530

    update1

---
+++ 6_ConditionalStatments/01_ifCondition.ts
@@ -0,0 +1,21 @@
+export{}
+
+//  If condition statment
+//It always returs ture or false 
+
+/* 
+if(condtion)
+{
+
+    //if the condtion is true this statments will be executed
+
+}
+*/
+
+//Example:
+
+let personAge:number = 20
+
+if(personAge>=18){
+    console.log("This person is eligible to vote")
+}
