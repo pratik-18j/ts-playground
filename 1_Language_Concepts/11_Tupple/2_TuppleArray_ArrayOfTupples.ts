2658024579a57cb064be5bdc1901335316161740
pratik-18j <jpartik1891@gmail.com>
2026-08-07 00:07:05 +0530

    Tupple

--- 1_Language_Concepts/11_Tupple/2_TuppleArray_ArrayOfTupples.ts
+++ 1_Language_Concepts/11_Tupple/2_TuppleArray_ArrayOfTupples.ts
@@ -4,4 +4,9 @@ let students:[number,string] [] = [[1,"Kelly"],[2,"Mac"],[3,"Josh"],[4,"Alice"]]
 
 console.log(students.length)
 console.log(students[1])        //[2,"Mac"]
-console.log(students[1][0])  
+
+let tp = students[1]  // tp does not store the actual primitive values directly inside itself. Instead, it stores a reference (memory address) that points to where the tuple array [2, "Mac"] lives in memory.
+
+console.log(tp[0])
+console.log(tp[1])
+
